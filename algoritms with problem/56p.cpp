7ef2778c4d4a01e68eaa69b15552e3c1c81af40a
KimHgYg <35590074+KimHgYg@users.noreply.github.com>
2018-09-13 17:27:46 +0900

    9/13
    0.h 배열
    56p 생각해보기 -> 피보나치, 재귀

---
+++ algoritms with problem/56p.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int function(int *, int);
+
+int main() {
+	int n;
+	unsigned int c;
+	cin >> n;
+	int *memo = (int *)malloc(n*(sizeof(int)));
+	memset(memo, 0, n * sizeof(int) + 4);
+	c = function(memo, n);
+	double p = c / pow(2.0, n);
+	cout << "경우의 수 : " << c << " 확률 : " << p;
+	getchar(); getchar();
+}
+
+int function(int *memo, int n) {
+	if (n == 1)
+		return memo[n] = 2;
+	else if (n == 2)
+		return memo[n] = 4;
+	else if (n == 3)
+		return memo[n] = 7;
+	if (memo[n] != 0)
+		return memo[n];
+	return memo[n] = (function(memo, n - 1) + function(memo, n - 2) + function(memo, n - 3));
+}
